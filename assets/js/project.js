44c322d66a0544320e6920f3e71925de916f2c85
IosamahI <1999.osamah.1@gmail.com>
2025-02-09 03:30:35 +0300

    Number 7

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,10 +1,17 @@
-AOS.init();
+AOS.init({
+  duration: 1000,
+  once: true
+});
+
+const projectcards = document.querySelector(".projectcards");
+const projectSearch = document.getElementById('projectSearch');
 /* Project Cards */
 
 const projectcards = document.querySelector(".projectcards");
 
-// Array of objects for projects
-const projects = [
+// Array of object for projects
+const projects  = [
+  
   {
     title: "python programming language",
     cardImage: "assets/images/project-page/python-programming-language.png",
@@ -13,6 +20,7 @@ const projects = [
     Previewlink: "",
     Githublink: "https://github.com/IosamaI/python-CS50p",
   },
+
   {
     title: "PDF Image to CSV Converter",
     cardImage: "assets/images/project-page/post-convert-pdf-to-csv.png",
@@ -83,7 +91,8 @@ const projects = [
     cardImage: "assets/images/project-page/Thteads.png",
     description:
       "Design-a-Threads-like-social-network-website",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
+    tagimg:
+      "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "https://github.com/IosamaI/Design-a-Threads-like-social-network-website",
   },
@@ -91,7 +100,8 @@ const projects = [
     title: "Movie-Search-API",
     cardImage: "assets/images/project-page/car.png",
     description: "This project is a simple web application that allows users to search for TV shows and movies using the TVmaze API.",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
+    tagimg:
+      "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "https://github.com/IosamaI/Movie-Search-API",
   },
@@ -99,7 +109,8 @@ const projects = [
     title: "Movie Recommendation System",
     cardImage: "assets/images/project-page/movie-recommendation.jpeg",
     description: "Built using Tensorflow.",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
+    tagimg:
+      "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "",
   },
@@ -107,15 +118,17 @@ const projects = [
     title: "Photo-Blog-web-page",
     cardImage: "assets/images/project-page/blog.png",
     description: "The Photo Blog project is a simple and visually appealing web page that showcases a collection of images in a blog",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
+    tagimg:
+      "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "https://github.com/IosamaI/Photo-Blog-web-page",
   }, 
-  {
+   {
     title: "Museum of Candy Website",
     cardImage: "assets/images/project-page/candy.png",
     description: "website aims to provide visitors with information about the museum, ticketing options",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
+    tagimg:
+      "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "https://github.com/IosamaI/Candy",
   },
@@ -123,11 +136,11 @@ const projects = [
     title: "Express-and-MongoDB",
     cardImage: "assets/images/project-page/ee.png",
     description: "This project is a basic web application built using Express.js, MongoDB, and EJS",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
+    tagimg:"https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "https://github.com/IosamaI/Express-and-MongoDB-",
   },
-  {
+   {
     title: "Ping Pong Scorekeeper Game",
     cardImage: "assets/images/project-page/ping.jpg",
     description: "A simple, interactive web application that allows two players to track scores for a ping pong game use js",
@@ -137,64 +150,47 @@ const projects = [
   },
 ];
 
-// Function for rendering project cards data
-const showCards = () => {
-  let output = "";
-  projects.forEach(({ title, cardImage, Previewlink, Githublink }) => {
-    output += `       
-      <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600">
-        <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-          <div class="header">
-          </div>
-          <div class="data">
-            <div class="content">
-              <div class="title-div">
-                <h1 class="title"><a href="#">${title}</a></h1>
+// function for rendering project cards data
+const createProjectCards = (filteredProjects) => {
+  let output = '';
+  filteredProjects.forEach(({ title, cardImage, description, Previewlink, Githublink, tags }) => {
+      output += `
+      <div class="col-12 col-md-6 col-lg-4 mb-4 skill-card" 
+           data-aos="zoom-in-up"
+           data-tags="${tags.join(',')}">
+          <div class="card h-100">
+              <img src="${cardImage}" class="card-img-top" alt="${title} project screenshot">
+              <div class="card-body">
+                  <h5 class="card-title">${title}</h5>
+                  <p class="card-text">${description}</p>
+                  <div class="d-flex justify-content-between">
+                      ${Previewlink ? 
+                          `<a href="${Previewlink}" class="btn btn-primary" target="_blank">Preview</a>` : 
+                          `<button class="btn btn-secondary" disabled>Preview</button>`}
+                      ${Githublink ? 
+                          `<a href="${Githublink}" class="btn btn-dark" target="_blank">GitHub</a>` : 
+                          `<button class="btn btn-secondary" disabled>GitHub</button>`}
+                  </div>
               </div>
-              <ul class="menu-content"><br>
-                <li>
-                  <a href="${Previewlink}" class="social-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor">
-                      <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-                      <line x1="8" y1="21" x2="16" y2="21"></line>
-                      <line x1="12" y1="17" x2="12" y2="21"></line>
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="${Githublink}" class="social-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
-                      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-            </div>
           </div>
-        </div>
       </div>`;
   });
   projectcards.innerHTML = output;
 };
 
-document.addEventListener("DOMContentLoaded", showCards);
+// Initial render
+createProjectCards(projects);
 
-function myFunction() {
-  // Declare variables
-  var input = document.getElementById("myInput").value;
-  input = input.toUpperCase();
-  var skillcard = document.getElementsByClassName("skill-card");
-  var card = document.getElementsByClassName("card");
-  var title = document.getElementsByClassName("title");
+// Search function
+const filterProjects = () => {
+  const searchTerm = projectSearch.value.toLowerCase();
+  const filtered = projects.filter(project => 
+      project.title.toLowerCase().includes(searchTerm) ||
+      project.description.toLowerCase().includes(searchTerm) ||
+      project.tags.some(tag => tag.toLowerCase().includes(searchTerm))
+  );
+  createProjectCards(filtered);
+};
 
-  // Loop through all project cards, and hide those that don't match the search query
-  for (var i = 0; i < title.length; i++) {
-    if (title[i].innerHTML.toUpperCase().includes(input)) {
-      skillcard[i].style.display = "";
-      card[i].style.display = "";
-    } else {
-      skillcard[i].style.display = "none";
-      card[i].style.display = "none";
-    }
-  }
-}
+// Event listeners
+projectSearch.addEventListener('input', filterProjects);
